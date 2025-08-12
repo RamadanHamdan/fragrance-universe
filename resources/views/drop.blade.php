458bc6aab464d7281c97922927bb1d7a2c5c610b
Ramadan <ramadanhamdan98@gmail.com>
2025-08-12 20:15:28 +0700

    mengubah file

--- resources/views/drop.blade.php
+++ resources/views/drop.blade.php
@@ -7,7 +7,7 @@
             <div class="max-w-4xl mx-auto p-8 rounded-xl bg-white search-box">
                 <h3 class="text-2xl font-semibold primary-text mb-6">Drop Your Fragrance</h3>
             <!-- Main modal -->
-                <div id="crud-modal">
+                <div id="crud-modal" aria-hidden="false">
                     <div class="max-w-4xl p-4 w-full max-h-full">
                         <!-- Modal content -->
                         <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-200">
@@ -16,33 +16,39 @@
                                 <h3 class="text-lg font-semibold text-gray-900 dark:text-black">
                                     Insert New Product
                                 </h3>
+                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-black" data-modal-toggle="crud-modal">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
                             </div>
                             <!-- Modal body -->
                             <form class="p-4 md:p-5">
                                 <div class="grid gap-4 mb-4 grid-cols-2">
                                     <div class="col-span-2">
                                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Perfume Name</label>
-                                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
                                     </div>
                                     <div class="col-span-2 sm:col-span-1">
                                         <label for="top-notes" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Top Notes</label>
-                                        <input type="text" name="top-notes" id="top-notes" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type top notes" required="">
+                                        <input type="text" name="top-notes" id="top-notes" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type top notes" required="">
                                     </div>
                                     <div class="col-span-2 sm:col-span-1">
                                         <label for="middle-notes" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Middle Notes</label>
-                                        <input type="text" name="middle-notes" id="middle-notes" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type middle notes" required="">
+                                        <input type="text" name="middle-notes" id="middle-notes" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type middle notes" required="">
                                     </div>
                                     <div class="col-span-2 sm:col-span-1">
                                         <label for="best-notes" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Best Notes</label>
-                                        <input type="text" name="best-notes" id="best-notes" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type best notes" required="">
+                                        <input type="text" name="best-notes" id="best-notes" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type best notes" required="">
                                     </div>
                                     <div class="col-span-2 sm:col-span-1">
                                         <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Price</label>
-                                        <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
+                                        <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
                                     </div>
                                     <div class="col-span-2 sm:col-span-1">
                                         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Category</label>
-                                        <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                        <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                             <option selected="">Select category</option>
                                             <option>Extrait de Perfume</option>
                                             <option>Eau de Perfume</option>
@@ -51,7 +57,7 @@
                                     </div>
                                     <div class="col-span-2 sm:col-span-1">
                                         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Size</label>
-                                        <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                        <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                             <option selected="">Select Size</option>
                                             <option>5ml</option>
                                             <option>10ml</option>
@@ -61,7 +67,7 @@
                                     </div>
                                     <div class="col-span-2">
                                         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Product Description</label>
-                                        <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Write product description here"></textarea>                    
+                                        <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Write product description here"></textarea>                    
                                     </div>
                                 </div>
                                 <button type="submit" class="text-black inline-flex items-center bg-gray-400 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-300 dark:hover:bg-gray-300 dark:focus:ring-gray-300">
